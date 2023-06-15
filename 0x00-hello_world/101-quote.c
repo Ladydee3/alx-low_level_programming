98fc84ba6dc0db4c800332145fc72afa4532d461
Ladydee3 <delisiletwala62@gmail.com>
2023-06-15 16:16:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: 0 (success)
+ * main - A C program that prints a line to the  stardard error
+ * return 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\* - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\* - Dora Korpar, 2015-10-19"));
-return (1);
-}
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}	
