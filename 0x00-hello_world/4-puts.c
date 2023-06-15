8033eeb371678f32eb3e2b5dcf3f2730bd0d774f
Ladydee3 <delisiletwala62@gmail.com>
2023-06-15 03:31:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A programe that printss a line with puts fuction
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
