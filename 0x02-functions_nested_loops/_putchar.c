ff3b1b48346b5bec1717ca071bcc34fdd06ecd7b
Ladydee3 <delisiletwala62@gmail.com>
2023-06-20 16:35:44 -0700

    10-add

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
-"I'm federal agent Jack Bauer, and today is the longest day of my life"
-
-=======
 #include <unistd.h>
 
-
 /**
- * _putchar -writes  the character c to stdout
- * @c: The  character to print
- * Return: On Success 1.
- * On error, -1 is returned, and errno is a set oppropriately.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is retuned, and error is set appropriately.
  */
 
 int _putchar(char c)
-
 {
 	return (write(1, &c, 1));
 }
->>>>>>> b472dfd2814f95da3ef69afb067740ac53c7db3a
