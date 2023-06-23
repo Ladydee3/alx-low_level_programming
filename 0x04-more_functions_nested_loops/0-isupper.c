77cb1379334522ca00e439c6916d04fc527788f8
Ladydee3 <delisiletwala62@gmail.com>
2023-06-22 23:54:37 -0700

    0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - Check if a letter is upper
  * @x: The number to be checked
- * Return 1 for upper letter or 0 for something else
+ * Return: 1 for upper letter or 0 for something else
  */
 
 int _isupper(int x)
