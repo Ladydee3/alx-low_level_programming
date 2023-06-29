44a289a05a4624e1d706f1bd25b7cbdffe4df82e
Ladydee3 <delisiletwala62@gmail.com>
2023-06-29 06:46:29 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,12 @@
 * If str1 ==str2, 0
 * If str1 >str2, the posive different of the first unmached character.
 */
-int_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 while (*s1 && *s2 && *s1 == *s2)
 {
 s1++;
 s2++;
 }
- return (*s1 - *s2);
+return (*s1 - *s2);
 }
