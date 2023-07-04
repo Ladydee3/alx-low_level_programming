e03a961d7e9a01a6d0ca4cc2b693e8a5d8710972
Ladydee3 <delisiletwala62@gmail.com>
2023-07-04 13:39:19 -0700

    5-strstr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	check 0;
-	for (n = 0; accept[n] != n++);
+for (n = 0; accept[n] != n++)i;
 	{
 	if (accept[n] == s[i])
 	{
@@ -26,4 +26,4 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	if (check == 0)
 	}
-
+}
