7c0aeab205661b99c6258712c97a305a0d51fe01
Ladydee3 <delisiletwala62@gmail.com>
2023-07-17 04:31:08 -0700

    2-print_dog.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - prints the name of the program
+ * main -writes the name of the program that prints its name
+ * if you rename the program it will print the new name withouth havng to compile it again
+ * you should not remove the path before the name of the program
+ *
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char * argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", *argv[0]);
+	(void)argc;
 	return (0);
-}	
+}
