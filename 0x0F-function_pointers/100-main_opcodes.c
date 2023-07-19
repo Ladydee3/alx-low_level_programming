cdd6b06fcf7d3f87645dc4af797903bcff1937d2
Ladydee3 <delisiletwala62@gmail.com>
2023-07-19 13:44:43 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int byte, i;
+	int bytes, i;
 	char *arr;
 
 	if (argc != 2)
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	printf("Error\n");
 	exit(1);
 	}
-	byte = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (byte < 0)
+	if (bytes < 0)
 	{
 	printf("Error\n");
 	exit(2);
@@ -28,11 +28,11 @@ int main(int argc, char *argv[])
 
 	arr = (char *)main;
 
-	for (i = 0; i < byte; i++)
+	for (i = 0; i < bytes; i++)
 	{
-	if (i == byte - 1)
+	if (i == bytes - 1)
 	{
-	printf("%02hhx\n ", arr[i]);
+	printf("%02hhx\n", arr[i]);
 	break;
 	}
 	printf("%02hhx ", arr[i]);
