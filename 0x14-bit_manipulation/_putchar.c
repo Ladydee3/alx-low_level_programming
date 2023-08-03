67076f2a41d3a3c0daba2074fbe9c69344ae0ddd
ladydee3 <delisiletwala62@gmail.com>
2023-08-03 05:40:34 -0700

    /b

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes charecter to standard outpit
+ * @c: character to be written
+ *
+ * Return: number of character written on success
+ * 1 is returned and errno i set correctly on  error
+ */
+
+int _putchar(char c)
+{
+putchar(c);
+return (1);
+}
+
